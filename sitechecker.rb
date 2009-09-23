413c0e68c2add5e81f9189b6b2e7f64c115a749e
Michael Larkin <mikelarkin@pixallent.com>
2009-09-23 17:40:55 -0400

    email updates

--- sitechecker.rb
+++ sitechecker.rb
@@ -12,8 +12,8 @@ require 'uri'
 ############### Settings ###############
 
 SITES = %w{www.fetchapp.com www.synctobase.com www.pixallent.com www.stealthpublisher.com www.fetarp.com ssd.com}
-FROM_EMAIL = "no-reply@pixallent.com"
-TO_EMAIL =  "mikelarkin@pixallent.com"
+FROM_EMAIL = "Pixallent SiteChecker <no-reply@pixallent.com>"
+TO_EMAIL =  "Pixallent Support <mikelarkin@pixallent.com>"
 SMTP_SERVER = "localhost"   
 
 ############### Mail Helper ###############
