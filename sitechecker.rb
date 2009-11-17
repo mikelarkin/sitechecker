7c3441ce0c23ffe6cf04678f7530926e36367e32
Michael Larkin <mikelarkin@pixellent.com>
2009-11-17 13:14:23 -0500

    updates to detect when the site is back up

--- sitechecker.rb
+++ sitechecker.rb
@@ -9,61 +9,81 @@ require 'net/http'
 require 'net/smtp'
 require 'uri'
 
+
 ############### Settings ###############
 
-SITES = %w{www.fetchapp.com www.synctobase.com www.pixallent.com}
-FROM_EMAIL = "Pixallent Site Checker <no-reply@pixallent.com>"
-TO_EMAIL =  "Pixallent Support <help@pixallent.com>"
+SITES = %w{www.stealthpublisher.net}
+FROM_EMAIL = "Pixellent Site Checker <no-reply@pixellent.com>"
+TO_EMAIL =  "Pixellent Support <mikelarkin@pixellent.com>"
 SMTP_SERVER = "localhost"
+DEBUG = false
 
 # true - will follow all redirects regardless of domain
 # false	- will raise error if redirected
-FOLLOW_REDIRECTS = false
-                
+FOLLOW_REDIRECTS = true
+
 # true - will allow a 503 (typical maintenance status code)
 # false	- will raise error if exact address does not return 2xx
 ALLOW_503 = true
 
 ############### Mail Helper ###############
 
-def send_email(from, to, subject, message) 
-	msg = <<END_OF_MESSAGE
-From: #{from}
-To: #{to}
-Subject: #{subject}
-	
-#{message}
-END_OF_MESSAGE
-	              
+def send_email(from, to, subject, message)
+  msg = "From: #{from}\nTo: #{to}\nSubject: #{subject}\n#{message}"
+
   Net::SMTP.start(SMTP_SERVER) do |smtp|
     smtp.send_message msg, from, to
   end
-end    
+end
+
+############### File Helpers ###############
+
+def send_up_alert(site, response)
+  if File.exist?("status/#{site}")
+    # Site was down, is now up
+    puts "--#{site} was down, is now back up, sending email" if DEBUG
+    File.delete("status/#{site}")
+    send_email(FROM_EMAIL, TO_EMAIL, "#{site} is UP", "Status: #{response.code}\n\n #{response.body}")
+  end
+end
+
+def send_down_alert(site, response)
+  if !File.exist?("status/#{site}")
+    puts "--Cannot reach #{site}, sending email" if DEBUG
+    File.new("status/#{site}", "w")
+    send_email(FROM_EMAIL, TO_EMAIL, "#{site} is DOWN", "Status: #{response.code}\n\n #{response.body}")
+  end
+end
 
 ############### Check Sites ###############
 
 SITES.each do |site|
-  url = URI.parse("http://#{site}/")
+  url = URI.parse("http://#{site}")
+  previous_location = nil
   begin
-  	 found = false
+    found = false
     until found
-      host, port = url.host, url.port if url.host && url.port
-      req = Net::HTTP::Get.new(url.path)
-      res = Net::HTTP.start(url.host, url.port) {|http|  http.request(req) } 
-      res.header['location'] ? url = URI.parse(res.header['location']) : found = true
+      response = Net::HTTP.get_response(site, "/")
+      previous_location = url
+      response.header['location'] ? url = URI.parse("#{response.header['location']}") : found = true
 
-		found = true if FOLLOW_REDIRECTS == false # Consider the initial request "found" if we're not following redirects
-    end    
+      found = true if FOLLOW_REDIRECTS == false # Consider the initial request "found" if we're not following redirects
+      found = true if previous_location == url # Check for infinite loop
+    end
+
     # Make sure the code is in the 200 range
-    unless (res.code.to_i >= 200 && res.code.to_i < 300) || (res.code.to_i == 503 && ALLOW_503 == true)
-      # If not, email a notice
-      puts "--Cannot reach #{site}, sending email--"
-      send_email(FROM_EMAIL, TO_EMAIL, "#{site} is DOWN", "Status: #{res.code}\n\n #{res.body}")
+    if (response.code.to_i >= 200 && response.code.to_i < 300) || (response.code.to_i == 503 && ALLOW_503 == true)
+		puts "-- #{site} is UP" if DEBUG
+      send_up_alert(site, response)
+    else
+      # If not, email a notice if not already sent and write temp file
+		puts "-- #{site} is DOWN" if DEBUG
+		send_down_alert(site, response)
     end
   rescue => e
     # Invalid
-    puts "--Cannot reach #{site}, sending email--"
-    send_email(FROM_EMAIL, TO_EMAIL, "#{site} is DOWN", "Message: #{e.message}")
+    puts "-- #{site} is DOWN or script is broken: #{e.message}" if DEBUG
+    send_down_alert(site, response)
   end
 
 end
